686c3ddac17b77de9a53dd7f49b29aa75d611433
Xkonti <xkonti@xkonti.tech>
2023-08-02 03:18:05 -0400

    Move to Vite (#1)
    * Move to Vite
    * Setup ESLint and Vitest
    * Add build check GH Action
    * Test route processor

--- src/router-service.ts
+++ src/router-service.ts
@@ -1,27 +1,27 @@
-import type { Router } from 'vue-router'
-import { RouteTarget } from './route-target'
+import type { Router } from 'vue-router';
+import { RouteTarget } from './route-target';
 
 export class RouterService {
-  private routerInstance: Router | null = null
+  private routerInstance: Router | null = null;
 
   public get router () {
     if (this.routerInstance == null) {
-      const message = 'Tried to access the router before it was assigned to the router service.'
-      console.error(message)
-      throw new Error(message)
+      const message = 'Tried to access the router before it was assigned to the router service.';
+      console.error(message);
+      throw new Error(message);
     } else {
-      return this.routerInstance
+      return this.routerInstance;
     }
   }
 
   public assignRouter (instance: Router) {
-    this.routerInstance = instance
+    this.routerInstance = instance;
   }
 
   public go (path: RouteTarget | string) {
-    const fullPath = path instanceof RouteTarget ? path.path : path
-    return routerService.router.push(fullPath)
+    const fullPath = path instanceof RouteTarget ? path.path : path;
+    return routerService.router.push(fullPath);
   }
 }
 
-export const routerService = new RouterService()
+export const routerService = new RouterService();
